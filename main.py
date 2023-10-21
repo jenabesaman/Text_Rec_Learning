c1106a1a166f530d6756925ae0cd796b821e12af
root <root@example.com>
2023-10-21 16:18:58 +0330

    testing

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import pytesseract
+import cv2
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+img=cv2.imread("Untitled.jpg")
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# cv2.imshow(winname="rgb",mat=rgb)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+# config_tesseract= "--tessdata-dir tessdata --psm 6"
+# text=pytesseract.image_to_string(image=rgb,lang="fas",config=config_tesseract)
+text=pytesseract.image_to_string(image=rgb,lang="fas")
+print(text)
